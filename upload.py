d5261e7df1fad140fe7edfb26022ffe9aaf24eb6
daisychain-jp <t.inamori@daisychain.jp>
2019-04-18 14:17:18 +0900

    enable upload.py to trim silence in sound

--- upload.py
+++ upload.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 import cgi
-import re
+import subprocess
+from datetime import datetime
 
 form = cgi.FieldStorage()
 
+log_fname = './log/{0}_upload.log'.format(datetime.now().strftime("%Y%m%d_%H%M%S"))
+ffmpeg = '/home/tinamori/usr/bin/ffmpeg'
+
 if "voice" in form:
     char = form.getvalue('char')
-    a_file = form["voice"]
-    out_fname = "./sound/%s.mp3" % char
-    with open(out_fname, mode='wb') as out_f:
-        out_f.write(a_file.file.read())
+    up_file = form["voice"]
+    trim_sound_fname = './sound/{}.mp3'.format(char)
+    orig_sound_fname = './sound_orig/{}.mp3'.format(char)
+    with open(orig_sound_fname, mode='wb') as work_f:
+        work_f.write(up_file.file.read())
+    with open(log_fname, mode='w') as log_f:
+        subprocess.run([ffmpeg, '-i', orig_sound_fname, '-af', 'silenceremove=start_periods=1:start_duration=0:start_threshold=-40dB:detection=peak', trim_sound_fname], stdout=log_f, stderr=log_f)
 
 print("Content-Type: text/html;")
 print("")
